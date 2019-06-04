ed471630b2dee85ef5a77326a8c92ad2aad85e8f
Youssef Hamdi <youssefhamdih11@gmail.com>
2019-06-04 05:02:41 +0200

    Add files via upload

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author Youssef hamdi
+ */
+public class Calculator {
+    
+    public float add(float x, float y)
+    {
+        return x+y;
+    }
+     public float subtract(float x, float y)
+    {
+        return x-y;
+    }
+      public float multibly(float x, float y)
+    {
+        return x*y;
+    }
+       public float divide(float x, float y)
+    {
+        return x/y;
+    }
+}
